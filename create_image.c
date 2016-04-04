2b38e2570d31773df37808ebd6ea6d32054210a1
Vincent Lefoulon <vincent.lefoulon@free.fr>
2016-04-04 09:50:36 +0200

    Comment code

--- create_image.c
+++ create_image.c
@@ -6,19 +6,22 @@
 #include "create_image.h"
 #include <stdio.h>
 
+// Retourne la composante rouge d'un pixel
 uint8_t get_r(uint32_t px)
 {
-  return px >> 16;
+    return px >> 16;
 }
 
+// Retourne la composante verte d'un pixel
 uint8_t get_g(uint32_t px)
 {
-  return px >> 8;
+    return px >> 8;
 }
 
+// Retourne la composante bleu d'un pixel
 uint8_t get_b(uint32_t px)
 {
-  return px;
+    return px;
 }
 
 void create_image_ppm(uint32_t *pic, int32_t size_x, int32_t size_y, char *filename)
@@ -26,26 +29,27 @@ void create_image_ppm(uint32_t *pic, int32_t size_x, int32_t size_y, char *filen
     FILE *f;
     f = fopen(filename, "w");
 
+    // En-tete, en ASCII
     fprintf(f, "P6\n");
     fprintf(f, "%d", size_x);
     fprintf(f, " ");
     fprintf(f, "%d", size_y);
     fprintf(f, "\n255\n");
 
+    // Pixels, en binaire
     uint64_t area = size_x * size_y;
     uint32_t px;
     uint8_t r, g, b;
 
-    for (uint64_t i = 0; i < area; i++)
-    {
-      px = pic[i];
-      r = get_r(px);
-      g = get_g(px);
-      b = get_b(px);
-      
-      fwrite(&r, sizeof(uint8_t), 1, f);
-      fwrite(&g, sizeof(uint8_t), 1, f);
-      fwrite(&b, sizeof(uint8_t), 1, f);
+    for (uint64_t i = 0; i < area; i++) {
+        px = pic[i];
+        r = get_r(px);
+        g = get_g(px);
+        b = get_b(px);
+        
+        fwrite(&r, sizeof(uint8_t), 1, f);
+        fwrite(&g, sizeof(uint8_t), 1, f);
+        fwrite(&b, sizeof(uint8_t), 1, f);
     }
 
     fclose(f);
